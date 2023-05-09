f94837e60a240bf7fae47012c54d14f0811f08ba
bebesolo29 <132704028+bebesolo29@users.noreply.github.com>
2023-05-08 19:38:02 -0500

    Delete page definitive directory

--- page definitive/js/index.js
+++
@@ -1,5 +0,0 @@
-const btnswitch = document.querySelector("#switch");
-btnswitch.addEventListener("click", ()=>{
-  document.body.classList.toggle("dark");
-  btnswitch.classList.toggle("active");
-});
